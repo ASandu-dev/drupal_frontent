6c3b8669dac05017aa089664f0969e7d2f5cb289
@AS <andrei.sandu@computerminds.co.uk>
2025-05-16 12:41:37 +0100

    pages1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["andreisandu.net"],
+    unoptimized: true,
   },
+  reactStrictMode: true,
+  basePath: isProd ? '/drupal_frontend' : '',
+  assetPrefix: isProd ? '/drupal_frontend/' : '',
+  output: 'export',
 };
 
 module.exports = nextConfig;
